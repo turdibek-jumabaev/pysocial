96b792ecf7e4aa1e79b130815c459e432c46ebdf
Turdibek <turdibekdev05@gmail.com>
2022-04-06 13:08:34 +0500

    get_title - funktsiyasi qo'shildi

--- uztube/__main__.py
+++ uztube/__main__.py
@@ -9,9 +9,17 @@ class YouTube:
         self.title = None
         self.video_url = None
 
-    def get_video_id(self):
+    def get_video_url(self):
         if self.url is None:
             raise Exception("URL kiritilmagan")
         else:
             self.video_url = self.url.split("=")[1]
             return self.video_url
+
+    def get_title(self):
+        if self.url is None:
+            raise Exception("URL kiritilmagan")
+        else:
+            self.title = requests.get(self.url).text.split(
+                "<title>")[1].split("</title>")[0]
+            return self.title
